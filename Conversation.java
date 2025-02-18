216629a326997d29282d322d41bab7610b918548
Sophia Klatell <sklatell@smith.edu>
2025-02-17 22:24:42 -0500

    Coded Conversation.java and included the reflection

--- Conversation.java
+++ Conversation.java
@@ -1,4 +1,9 @@
 
+import java.util.Scanner; //import
+import java.util.Random; //import random
+import java.util.ArrayList; // import the ArrayList class
+//   https://www.geeksforgeeks.org/generating-random-numbers-in-java/  -- How to use randoms in Java
+
 class Conversation implements Chatbot {
 
   // Attributes 
@@ -13,7 +18,21 @@ class Conversation implements Chatbot {
   /**
    * Starts and runs the conversation with the user
    */
+
+  ArrayList<String> full_convo = new ArrayList<String>();
   public void chat() {
+    System.out.println("Hi there! What's on your mind?");
+    for (int i = 0; i <2; i++) {
+      Scanner user_response = new Scanner(System.in);
+      String line = user_response.nextLine();
+      full_convo.add(line);
+      String answer = respond(line);
+      System.out.println(answer);
+      full_convo.add(answer);
+      
+    }
+    System.out.println("See ya!");
+    
 
   }
 
@@ -21,7 +40,13 @@ class Conversation implements Chatbot {
    * Prints transcript of conversation
    */
   public void printTranscript() {
-
+    System.out.println("");
+    System.out.println("TRANSCRIPT");
+    System.out.println("Hi there! What's on your mind?");
+    for (String i : full_convo) {
+      System.out.println(i);
+    }
+    System.out.println("See ya!");
   }
 
   /**
@@ -30,9 +55,50 @@ class Conversation implements Chatbot {
    * @return mirrored or canned response to user input  
    */
   public String respond(String inputString) {
-    String returnString = ""; 
+    Integer counter = 0;
+    String responces [] = {"Mhmm", "...Right", "Okay"};
+
+
+    // Mirrors the users answer while changing pronouns. 
+    if (inputString.contains("I")) {
+      inputString = inputString.replace("I", "you");
+      counter += 1;
+    }
+    else if (inputString.contains("you")) {
+      inputString = inputString.replace("you", "I");
+      counter += 1;
+    }
+    if (inputString.contains("me")) {
+      inputString = inputString.replace("me", "you");
+      counter += 1;
+    }
+
+    if (inputString.contains("am")) {
+      inputString = inputString.replace("am", "are");
+      counter += 1;
+    }
+
+    if (inputString.contains("my")) {
+      inputString = inputString.replace("my", "your");
+      counter += 1;
+    }
+    else if (inputString.contains("your")) {
+      inputString = inputString.replace("your", "my");
+      counter += 1;
+    }
+
+
+    //Checks to make sure the responce isn't mirrored and then chooses a random responce
+    if (counter == 0) {
+      Random rand = new Random();
+      int rand_int1 = rand.nextInt(2);
+      inputString = responces[rand_int1];
+    }
+
+    String returnString = inputString; 
     return returnString; 
   }
+
 
   public static void main(String[] arguments) {
 
--- reflection.md
+++ reflection.md
@@ -5,3 +5,12 @@ Use this file to record your reflection on this assignment.
 - What are your initial impressions of Java? 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+
+    I came into class on our snow day to begin A3. This was extremely necessary because I could not get Java to run at first (Thank you Ab for helping 
+    me figure out the problem!) Java is definitly more difficult for me than Python just in the sense that it's less intuitive. For example I kept writing
+    "print()" when I wanted to print something, getting confused as to how a simple print didn't work, and then realizing I had to write the whole 
+    "System.out.println()" out. It, of course, wasn't a serious issue, but still it was annoying how often it happened. However, the two programming
+    languages, despite their differences, do have generally the same functions (they are capable of doing the same things). I feel like it's very valuable
+    to know several programming languages -- especially for later in your career -- but at my current level it's mostly stylistically not functionally different.
+    I would advice new students to just take it slow, learning a new language is incremental and w3schools is there for a reason!
